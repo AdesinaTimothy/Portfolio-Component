91e821fde2b727c249de140394a7264a4390961a
Timothy Adesina <timobest121@gmail.com>
2024-12-10 23:52:54 +0100

    add on

--- src/AddTagsComponent.jsx
+++ src/AddTagsComponent.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import PlusIcon from "./assets/plus.svg"
 import CancelIcon from "./assets/cancel.svg"
 import ErrorIcon2 from "./assets/error2.svg"
+import ErrorDiv from "./ErrorDiv.jsx";
+
 
 
 export default function AddTagsComponent() {
@@ -10,8 +12,10 @@ export default function AddTagsComponent() {
     const [tagsArray, setTagsArray] = useState([]);
     const [initialLoad, setInitialLoad] = useState(true)
     const maxTags = 8;
+    const [showError, setShowError] = useState(false);
+    const [emptyTag, setEmptyTag] = useState (false);
 
-
+    
     // useEffect (() => {
     //     const savedTags = localStorage.getItem('TagsTodo')
     //     if(savedTags) {
@@ -29,7 +33,7 @@ export default function AddTagsComponent() {
 
     // if (tags.trim() && !tagsArray.includes(tags.trim())) {
     //     setTagsArray([...tagsArray, tags.trim()]);
-    //     setTags('');
+    //     setTags('');lkkkk
     // }
 
 
@@ -37,13 +41,18 @@ export default function AddTagsComponent() {
         e.preventDefault()
         
         if (tagsArray.length >= maxTags) {
-            alert ("You can only add a maximum of 8 tags")
+            // <div className="max-tags-message">
+            //     <p>Maximun input of Tags reached</p>
+            // </div>
+            setShowError(true)
+            
         } else {
             if (tags.trim() !== "") {
                 setTagsArray([...tagsArray, tags.trim()])
             setTags('')
+            setEmptyTag(false)
             } else {
-                alert('Empty tag cannot be added')
+                setEmptyTag(true)
             }
     
         }
@@ -53,6 +62,7 @@ export default function AddTagsComponent() {
     const deletetag = (index) => {
         setTagsArray(tagsArray.filter((_, ind) => 
             ind !== index))
+        setShowError(false)
     }
 
   return (
@@ -79,6 +89,20 @@ export default function AddTagsComponent() {
         </div>
         </form>
 
+        {showError && (
+                    <div className="max-tags-error">
+                        <p>Maximum number of tags reached</p>
+                    </div>
+        )}
+
+        {emptyTag && (
+                    <div className="empty-tags">
+                        <p>Empty tags can't be added</p>
+                    </div>
+        )}
+
+
+
         <div className="tag-container">
         {tagsArray.map ((tags, index) => (
             <div key={index} className="tag-div">
@@ -89,6 +113,8 @@ export default function AddTagsComponent() {
             </div>
         ))}
         </div>
+
+        
 
         <form action="" className="access-form">
             <p>Members with access</p>
---
+++ src/ErrorDiv.jsx
@@ -0,0 +1,8 @@
+
+export default function ErrorDiv() {
+  return (
+    <div>
+      <p>Max number of tag is 8</p>
+    </div>
+  )
+}
